2599eefe4e9881aec0f0a898532e14343c64e062
MichalJ245 <michaljaruga7002@gmail.com>
2025-05-19 12:29:45 +0200

    Add files via upload

--- src/lib/Components/Listing.svelte
+++ src/lib/Components/Listing.svelte
@@ -9,9 +9,8 @@
 	}>;
 	const getData = async () => {
 		const response = await fetch('http://192.168.0.216:8000/users/api/submissions/');
-
 		const json = await response.json();
-
+		console.log(json);
 		listOfItems = json;
 	};
 
@@ -23,14 +22,14 @@
 <div class="flex flex-col justify-center">
 	{#if listOfItems}
 		{#each listOfItems as item}
-			<div class="mx-auto my-6 w-64 rounded-[30px] bg-gray-300 p-4">
-				<p><span class="font-bold text-purple-500">ID:</span> {item.id}</p>
-				<p><span class="font-bold text-purple-500">Name:</span> {item.name}</p>
-				<p><span class="font-bold text-purple-500">Email:</span> {item.email}</p>
+			<div class="mx-auto my-6 w-64 rounded-[30px] bg-gray-300 p-4 dark:bg-gray-600 dark: font-white">
+				<p><span class="font-bold text-purple-500 dark:text-purple-200">ID:</span> {item.id}</p>
+				<p><span class="font-bold text-purple-500 dark:text-purple-200">Name:</span> {item.name}</p>
+				<p><span class="font-bold text-purple-500 dark:text-purple-200">Email:</span> {item.email}</p>
 				{#if item.city}
-					<p><span class="font-bold text-purple-500">City:</span> {item.city}</p>
+					<p><span class="font-bold text-purple-500 dark:text-purple-200">City:</span> {item.city}</p>
 				{/if}
-				<p><span class="font-bold text-purple-500">Birthdate:</span> {item.birthdate}</p>
+				<p><span class="font-bold text-purple-500 dark:text-purple-200">Birthdate:</span> {item.birthdate}</p>
 			</div>
 		{/each}
 	{:else}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,24 @@
 <script lang="ts">
+	import {onMount} from 'svelte';
 	import '../app.css';
+	let dark = $state(false);
+	onMount(()=>{
+	dark = localStorage.getItem('theme') === 'dark' || false;
+	console.log(localStorage.getItem('theme'));
+	document.documentElement.classList.toggle('dark',dark);
+	});
+function toggleDarkMode()
+{
+	dark = !dark;
+	document.documentElement.classList.toggle('dark',dark);
+	localStorage.setItem('theme',dark ? 'dark':'light');
+}
 
 	let { children } = $props();
 </script>
 
-<a href="/add-age" class="m-2 rounded-[50px] bg-purple-500 p-2 text-white">add age</a>
+<a href="/add-age" class="m-2 rounded-[50px] bg-purple-500 p-2 text-white dark:text-black">add age</a>
 <a href="/listing" class="m-2 rounded-[50px] bg-purple-500 p-2 text-white">listing</a>
+<button onclick={toggleDarkMode} class="bg-black text-white p-2 rounded-[10px]">Dark Mode</button>
 
 {@render children()}
