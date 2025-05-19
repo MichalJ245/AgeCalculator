6598e4f0a25ea6c823a86d56053697fb501c363b
MichalJ245 <michaljaruga7002@gmail.com>
2025-05-19 14:57:54 +0200

    Add files via upload

--- src/lib/Components/Listing.svelte
+++ src/lib/Components/Listing.svelte
@@ -1,37 +1,54 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	let listOfItems: Array<{
+		let listOfItems: Array<{
 		id: number;
 		name: string;
 		email: string;
-		city?: string;
+		city: string | null;
 		birthdate: string;
-	}>;
+	}> = $state([]);
+	let currentRow: number = $state(0);
 	const getData = async () => {
 		const response = await fetch('http://192.168.0.216:8000/users/api/submissions/');
 		const json = await response.json();
-		console.log(json);
 		listOfItems = json;
+		console.log(listOfItems[currentRow].city);
 	};
 
 	onMount(() => {
 		getData();
 	});
+	function previous()
+	{
+		if(currentRow > 0)
+	{
+		currentRow--;
+	}
+	}
+	function next()
+	{
+		if(currentRow < listOfItems.length-1)
+	{
+		currentRow++;
+	}
+	}
 </script>
-
+<h1 class="text-center font-bold text-3xl">Dane z tabeli</h1>
 <div class="flex flex-col justify-center">
-	{#if listOfItems}
-		{#each listOfItems as item}
+	{#if listOfItems[currentRow]}
 			<div class="mx-auto my-6 w-64 rounded-[30px] bg-gray-300 p-4 dark:bg-gray-600 dark: font-white">
-				<p><span class="font-bold text-purple-500 dark:text-purple-200">ID:</span> {item.id}</p>
-				<p><span class="font-bold text-purple-500 dark:text-purple-200">Name:</span> {item.name}</p>
-				<p><span class="font-bold text-purple-500 dark:text-purple-200">Email:</span> {item.email}</p>
-				{#if item.city}
-					<p><span class="font-bold text-purple-500 dark:text-purple-200">City:</span> {item.city}</p>
+				<p><span class="font-bold text-purple-500 dark:text-purple-200">ID:</span> {listOfItems[currentRow].id}</p>
+				<p><span class="font-bold text-purple-500 dark:text-purple-200">Name:</span> {listOfItems[currentRow].name}</p>
+				<p><span class="font-bold text-purple-500 dark:text-purple-200">Email:</span> {listOfItems[currentRow].email}</p>
+				{#if listOfItems[currentRow].city}
+					<p><span class="font-bold text-purple-500 dark:text-purple-200">City:</span> {listOfItems[currentRow].city}</p>
 				{/if}
-				<p><span class="font-bold text-purple-500 dark:text-purple-200">Birthdate:</span> {item.birthdate}</p>
+				<p><span class="font-bold text-purple-500 dark:text-purple-200">Birthdate:</span> {listOfItems[currentRow].birthdate}</p>
+				<div>
+				<button onclick={previous} class="p-4 bg-purple-500 rounded-[10px] mr-32">&lt;</button>
+				<button onclick={next} class="p-4 bg-purple-500 rounded-[10px]">&gt;</button>
+				</div>
 			</div>
-		{/each}
 	{:else}
 		<p>Brak danych</p>
 	{/if}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import {onMount} from 'svelte';
 	import '../app.css';
+	import {scale} from 'svelte/transition';
 	let dark = $state(false);
 	onMount(()=>{
 	dark = localStorage.getItem('theme') === 'dark';
@@ -12,12 +13,19 @@ function toggleDarkMode()
 	document.documentElement.classList.toggle('dark',dark);
 	localStorage.setItem('theme',dark ? 'dark':'light');
 }
-
+let navVisible = $state(true);
+function toggleNav()
+{
+navVisible = !navVisible;
+}
 	let { children } = $props();
 </script>
-
-<a href="/add-age" class="m-2 rounded-[50px] bg-purple-500 p-2 text-white dark:text-black">add age</a>
-<a href="/listing" class="m-2 rounded-[50px] bg-purple-500 p-2 text-white">listing</a>
-<button onclick={toggleDarkMode} class="bg-black text-white p-2 rounded-[10px]">Dark Mode</button>
-
+<button onclick={toggleNav} class="bg-purple-500 p-2 text-white">Menu</button>
+{#if navVisible}
+<nav id="nav" class="flex flex-col w-24 absolute m-4" transition:scale>
+<a href="/add-age" class="bg-purple-500 p-2 text-white dark:text-black border-2 border-purple-500 hover:border-y-gray-500">add age</a>
+<a href="/listing" class="bg-purple-500 p-2 text-white border-2 border-purple-500 hover:border-y-gray-500">listing</a>
+<button onclick={toggleDarkMode} class="bg-black text-white p-2 border-2 border-black hover:border-y-gray-600">Dark Mode</button>
+</nav>
+{/if}
 {@render children()}
