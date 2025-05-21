3a4c92c749eac20df33336698a81796ee0fec16f
MichalJ245 <michaljaruga7002@gmail.com>
2025-05-21 11:53:11 +0200

    Add files via upload

--- src/lib/Components/Listing.svelte
+++ src/lib/Components/Listing.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { twMerge } from 'tailwind-merge';
 	import { onMount } from 'svelte';
 	let listOfItems: Array<{
 		id: number;
@@ -7,93 +8,120 @@
 		city: string | null;
 		birthdate: string;
 	}> = $state([]);
-	function sort() {
-		let handler = document.getElementById('sort') as HTMLSelectElement;
-		let sortValue = handler.value;
-		switch (sortValue) {
-			case 'id':
-				listOfItems.sort((a, b) => a.id - b.id);
-				break;
-			case 'name':
-				listOfItems.sort((a, b) => a.name.localeCompare(b.name));
-				break;
-			case 'email':
-				listOfItems.sort((a, b) => a.name.localeCompare(b.name));
-				break;
-			case 'birthdate':
-				listOfItems.sort(
-					(a, b) => new Date(a.birthdate).getTime() - new Date(b.birthdate).getTime()
-				);
-				break;
-			case 'city':
-				listOfItems.sort((a, b) => {
-					if (!a.city) return 1;
-					if (!b.city) return -1;
-					return a.city.localeCompare(b.city);
-				});
-		}
-	}
-	let currentRow: number = $state(0);
+	var jsonFile: any;
+	let next: any = $state();
+	let previous: any = $state();
+	let page: number = $state(1);
+	let size: number = $state(1);
+	let dataCount: number;
+	const tableStyle = 'border-1 dark:border-white dark:text-white transition-colors duration-1000 border-collapse text-center';
 	const getData = async () => {
-		const response = await fetch('http://192.168.0.216:8000/users/api/submissions/?sort_by=name&page=1&per_page=2');
-		const json = await response.json();
-		listOfItems = json;
-		console.log(json);
+		const response = await fetch('http://192.168.0.216:8000/users/api/submissions/?ordering=id&page='+page+'&page_size='+size);
+		jsonFile = await response.json();
+		console.log(jsonFile.results);
+		listOfItems = jsonFile.results;
+		next = jsonFile.next;
+		previous = jsonFile.previous;
+		dataCount = jsonFile.count;
+		checkPrev();
+		checkNext();
 	};
-
 	onMount(() => {
 		getData();
 	});
-	function previous() {
-		if (currentRow > 0) {
-			currentRow--;
-		}
+	let nextRight: boolean = $state(true);
+	let nextLeft: boolean = $state(false);
+	function checkPrev()
+	{
+		if(previous)
+	{
+		nextLeft = true;
 	}
-	function next() {
-		if (currentRow < listOfItems.length - 1) {
-			currentRow++;
-		}
+	else
+	{
+		nextLeft= false;
+	}
+	}
+	function checkNext()
+	{
+		if(next)
+	{
+		nextRight = true;
+	}
+	else
+	{
+		nextRight= false;
+	}
+	}
+	function nextPage()
+	{
+		page++;
+		checkNext();
+		getData();
+	}
+	function prevPage()
+	{
+		page--;
+		checkPrev();
+		getData();
+	}
+	function buttonArrow(side: boolean) {
+		let buttonActive = 'bg-purple-500 text-white dark:bg-purple-600';
+		let buttonNotActive = 'bg-gray-200 text-gray-800 dark:bg-gray-500 dark:text-white';
+		return twMerge('inline-block w-16 p-4 rounded-[10px] m-2 text-3xl', side ? buttonActive : buttonNotActive);
+	}
+	function setSize()
+	{
+		let handler = document.getElementById('count') as HTMLInputElement;
+		size = parseInt(handler.value);
+		if(size > dataCount)
+	{
+		alert("There are less records in the database than "+size);
+	}
+	else
+	{
+		page = 1;
+		getData();
+	}
 	}
 </script>
 
-<h1 class="text-center text-3xl font-bold dark:text-white transition-colors duration-1000">Data from table</h1>
+<h1 class="text-center text-3xl font-bold transition-colors duration-1000 dark:text-white transition-colors duration-1000">
+	Data from table
+</h1>
 <div class="flex flex-col justify-center">
-	{#if listOfItems[currentRow]}
-		<div class="dark: font-white mx-auto my-6 w-64 rounded-[30px] bg-gray-300 p-4 dark:bg-gray-600 transition-colors duration-1000">
-			<p class="dark:text-white transition-colors duration-1000">
-				<span class="font-bold text-purple-500 dark:text-purple-600 transition-colors duration-1000">ID:</span>
-				{listOfItems[currentRow].id}
-			</p>
-			<p class="dark:text-white transition-colors duration-1000">
-				<span class="font-bold text-purple-500 dark:text-purple-600 transition-colors duration-1000">Name:</span>
-				{listOfItems[currentRow].name}
-			</p>
-			<p class="dark:text-white transition-colors duration-1000">
-				<span class="font-bold text-purple-500 dark:text-purple-600 transition-colors duration-1000">Email:</span>
-				{listOfItems[currentRow].email}
-			</p>
-			<p class="dark:text-white transition-colors duration-1000">
-				<span class="font-bold text-purple-500 dark:text-purple-600 transition-colors duration-1000">City:</span>
-				{listOfItems[currentRow].city ? listOfItems[currentRow].city : 'null'}
-			</p>
-			<p class="dark:text-white transition-colors duration-1000">
-				<span class="font-bold text-purple-500 dark:text-purple-600 transition-colors duration-1000">Birthdate:</span>
-				{listOfItems[currentRow].birthdate}
-			</p>
-			<div>
-				<button onclick={previous} class="mr-32 rounded-[10px] bg-purple-500 p-4 dark:bg-purple-600 transition-colors duration-1000">&lt;</button>
-				<button onclick={next} class="rounded-[10px] bg-purple-500 p-4 dark:bg-purple-600 transition-colors duration-1000">&gt;</button>
-			</div>
-			<p>Sort by:</p>
-			<select class="rounded-[5px] border-2 bg-white dark:bg-gray-700 dark:text-white transition-colors duration-1000" id="sort" onchange={sort}>
-				<option value="id">ID</option>
-				<option value="name">Name</option>
-				<option value="email">Email</option>
-				<option value="city">City</option>
-				<option value="birthdate">Birthdate</option>
-			</select>
+	{#if listOfItems}
+		<table class={tableStyle}>
+			<thead class={tableStyle}>
+				<tr class={tableStyle}>
+					<th>ID</th>
+					<th>Name</th>
+					<th>Email</th>
+					<th>Birthdate</th>
+					<th>City</th>
+				</tr>
+			</thead>
+			<tbody class={tableStyle}>
+				{#each listOfItems as item}
+					<tr class={tableStyle}>
+						<td class="border-1">{item.id}</td>
+						<td class="border-1">{item.name}</td>
+						<td class="border-1">{item.email}</td>
+						<td class="border-1">{item.birthdate}</td>
+						<td class="border-1">{item.city}</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+		<div class="flex justify-center">
+		<button class={buttonArrow(nextLeft)} onclick={() => {if(nextLeft){prevPage();}}}>&lt;</button>
+		<button class={buttonArrow(nextRight)} onclick={() => {if(nextRight){nextPage();}}}>&gt;</button>
+		</div>
+		<div class="flex justify-center">
+		<input type="number" id="count" class="border-1 w-32 dark:bg-gray-900 dark:text-white transition-colors duration-1000">
+		<button onclick={setSize} class="p-2 bg-purple-500 rounded-[10px] text-white dark:bg-purple-600 transition-colors duration-1000">Set</button>
 		</div>
 	{:else}
-		<p class="text-center">No datas, or connection failed</p>
+		<p class="text-center">No data, or connection failed</p>
 	{/if}
 </div>
